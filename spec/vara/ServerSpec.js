9265b4083afe8c6e49e735097481d6e679b888a4
John Kristian <jmkristian@gmail.com>
2023-03-27 10:08:28 -0700

    Prevent specs from interfering with each other.

--- spec/vara/ServerSpec.js
+++ spec/vara/ServerSpec.js
@@ -1,18 +1,8 @@
 const VARA = require('../../server.js');
-const Bunyan = require('bunyan');
 const MockNet = require('../mockNet/mockNet.js');
-const Net = require('net');
-const Readline = require('readline');
 const sinon = require('sinon');
-const Stream = require('stream');
-const util = require('util');
 
-const ECONNREFUSED = MockNet.ECONNREFUSED;
-const ETIMEDOUT = MockNet.ETIMEDOUT;
-const exposePromise = MockNet.exposePromise;
 const log = MockNet.log;
-const newError = MockNet.newError;
-
 const MockVersion = 'VERSION mock\r';
 
 class happyNet extends MockNet.mockNet {
@@ -49,82 +39,97 @@ class listenFails extends happyNet {
     }
 }
 
-class LineConsumer {
-    constructor(consume) {
-        this.consume = consume;
-    }
-    fromVARA(line) {
-        this.consume(line);
-    }
+function beginSpec(spec, description) {
+    log.debug('begin spec should %s %s', description, spec.serverOptions);
+    spec.description = description;
+}
+
+function endSpec(spec) {
+    log.debug('end spec should %s', spec.description);
 }
 
 describe('Server', function() {
 
     const sandbox = sinon.createSandbox();
-    let serverOptions, server
 
     beforeEach(function() {
         const that = this;
-        serverOptions = {
+        this.serverOptions = {
             host: Math.floor(Math.random() * (1 << 20)).toString(36),
             port: Math.floor(Math.random() * ((1 << 16) - 1)) + 1,
             Net: new happyNet(),
             logger: log,
         };
-        server = new VARA.Server(serverOptions);
+        this.server = new VARA.Server(this.serverOptions);
     });
 
     afterEach(function() {
+        if (this.server) this.server.close();
+        endSpec(this);
         sandbox.restore();
     });
 
     it('should not close when closed', function() {
-        server.close(function(err) {
-            expect(err).toBeTruthy();
+        beginSpec(this, 'not close when closed');
+        const server = this.server;
+        const closed = new Promise(function(resolve, reject) {
+            server.close(function(err) {
+                expect(err).toBeTruthy();
+                resolve();
+            });
         });
+        return expectAsync(closed).toBeResolved();
     });
 
     it('should require TNC port', function() {
+        beginSpec(this, 'require TNC port');
         try {
             new VARA.Server();
             fail('no options');
         } catch(err) {
         }
         try {
-            new VARA.Server({host: serverOptions.host});
+            new VARA.Server({host: this.serverOptions.host});
             fail('no options.port');
         } catch(err) {
         }
     });
 
     it('should require the VARA host', function() {
+        beginSpec(this, 'require the VARA host');
         try {
-            server.listen({});
+            this.server.listen({spec: 'should require the VARA host'});
             fail('no options.host');
         } catch(err) {
         }
     });
 
     it('should not listen when listening', function() {
-        var actual = null;
-        server.listen({host: ['N0CALL']});
-        try {
-            server.listen({host: ['N0CALL']});
-            fail();
-        } catch(err) {
-            actual = err;
-        }
-        expect(actual).toEqual(jasmine.objectContaining({
-            code: 'ERR_SERVER_ALREADY_LISTEN',
-        }));
+        beginSpec(this, 'not listen when listening');
+        const server = this.server;
+        const listening = new Promise(function(resolve, reject) {
+            server.listen({host: ['N0CALL']}, function() {
+                resolve();
+            });
+            try {
+                server.listen({host: ['N0CALL']});
+                fail();
+            } catch(err) {
+                expect(err).toEqual(jasmine.objectContaining({
+                    code: 'ERR_SERVER_ALREADY_LISTEN',
+                }));
+            }
+        });
+        return expectAsync(listening).toBeResolved();
     });
 
     it('should callback from listening', function() {
-        log.debug('Server should callback from listening');
+        beginSpec(this, 'callback from listening');
+        const server = this.server;
         const listening = new Promise(function(resolve, reject) {
             server.listen({host: 'N0CALL'}, function() {
                 if (server.listening) {
-                    setTimeout(resolve, 500);
+                    setTimeout(resolve, 100);
                     // The timeout isn't really neccessary, but
                     // it makes the log output more interesting.
                 } else {
@@ -136,7 +141,8 @@ describe('Server', function() {
     });
 
     it('should emit listening', function() {
-        log.debug('Server should emit listening');
+        beginSpec(this, 'emit listening');
+        const server = this.server;
         const listening = new Promise(function(resolve, reject) {
             server.on('listening', function() {
                 if (server.listening) {
@@ -151,27 +157,30 @@ describe('Server', function() {
     });
 
     it('should connect to TNC', function() {
-        log.debug('Server should connect to TNC');
+        beginSpec(this, 'connect to TNC');
+        const server = this.server;
+        const expectOptions = {
+            host: this.serverOptions.host,
+            port: this.serverOptions.port,
+        };
         const connectSpy = sandbox.spy(MockNet.mockSocket.prototype, 'connect');
         const connected = new Promise(function(resolve, reject) {
             server.listen({host: ['N0CALL']}, function() {
                 // Give the server some time to connect the socket.
                 setTimeout(function() {
                     expect(connectSpy.calledOnce).toBeTruthy();
-                    expect(connectSpy.getCall(0).args[0])
-                        .toEqual(jasmine.objectContaining({
-                            host: serverOptions.host,
-                            port: serverOptions.port,
-                        }));
+                    expect(connectSpy.getCall(0).args[0]).toEqual(
+                        jasmine.objectContaining(expectOptions));
                     resolve();
-                }, 500);
+                }, 100);
             });
         });
         return expectAsync(connected).toBeResolved();
     });
 
     it('should disconnect from TNC', function() {
-        log.debug('Server should disconnect from TNC');
+        beginSpec(this, 'should disconnect from TNC');
+        const server = this.server;
         const destroySpy = sandbox.spy(MockNet.mockSocket.prototype, 'destroy');
         const closed = new Promise(function(resolve, reject) {
             server.on('close', function(err) {
@@ -189,10 +198,10 @@ describe('Server', function() {
     });
 
     it('should report no TNC', function() {
-        log.debug('Server should report no TNC');
+        beginSpec(this, 'report no TNC');
         const spec = this;
-        server = new VARA.Server(Object.assign(
-            {}, serverOptions,
+        const server = new VARA.Server(Object.assign(
+            {}, this.serverOptions,
             {Net: new MockNet.noTNC(spec)}
         ));
         const closed = new Promise(function(resolve, reject) {
@@ -200,7 +209,7 @@ describe('Server', function() {
                 reject('listening');
             });
             server.on('error', function(err) {
-                expect(err.code).toEqual(ECONNREFUSED);
+                expect(err.code).toEqual(MockNet.ECONNREFUSED);
                 resolve();
             });
             server.listen({host: 'N0CALL'});
@@ -209,10 +218,10 @@ describe('Server', function() {
     });
 
     it('should report no TNC host', function() {
-        log.debug('Server should report no TNC host');
+        beginSpec(this, 'report no TNC host');
         const spec = this;
-        server = new VARA.Server(Object.assign(
-            {}, serverOptions,
+        const server = new VARA.Server(Object.assign(
+            {}, this.serverOptions,
             {Net: new MockNet.noTNCHost(spec)}
         ));
         const closed = new Promise(function(resolve, reject) {
@@ -221,7 +230,7 @@ describe('Server', function() {
             });
             server.on('error', function(err) {
                 log.debug('error %o from server', err);
-                expect(err.code).toEqual(ETIMEDOUT);
+                expect(err.code).toEqual(MockNet.ETIMEDOUT);
                 resolve();
             });
             server.listen({host: 'N0CALL'});
@@ -230,9 +239,10 @@ describe('Server', function() {
     });
 
     it('should not listen if TNC refuses', function() {
+        beginSpec(this, 'not listen if TNC refuses');
         const spec = this;
-        server = new VARA.Server(Object.assign(
-            {}, serverOptions,
+        const server = new VARA.Server(Object.assign(
+            {}, this.serverOptions,
             {Net: new listenFails(spec)}
         ));
         const closed = new Promise(function(resolve, reject) {
